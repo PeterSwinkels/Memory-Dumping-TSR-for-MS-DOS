85e8b3c93d9e9849ff96fa04866d5d014144ab76
Peter Swinkels <peter_swinkels@hotmail.com>
2021-06-20 21:16:49 +0200

    Add files via upload

--- mmdmptsr.asm
+++ mmdmptsr.asm
@@ -1,6 +1,28 @@
-; Memory Dumping TSR for MS-DOS v1.01 - By: Peter Swinkels, ***2021***
+; Memory Dumping TSR for MS-DOS v1.02 - By: Peter Swinkels, ***2021***
 ORG 0x0100                ; Indicates that all relative pointers to data are moved forward by 0x0100 bytes.
 
+JMP Main
+
+TSR:
+PUSHA                     ; Saves the registers.
+PUSH DS                   ;
+PUSH ES
+
+PUSH FS                   ; Restores the data segment register.
+POP DS                    ;
+
+%INCLUDE "Memdump.asm"    ; Includes the TSR's main body.
+
+POP ES                    ; Restores the registers.
+POP DS                    ;
+POPA                      ;
+
+INT RedirectedTo          ; Calls the redirected interrupt.
+
+IRET                      ; Returns.
+EndTSR:
+
+Main:
 RedirectedFrom EQU 0x08   ; Defines the interrupt to be redirected.
 RedirectedTo EQU 0xFF     ; Defines the redirected interrupt's new vector.
 
@@ -35,30 +57,11 @@ MOV AL, RedirectedFrom    ;
 INT 0x21                  ;
 
 MOV AX, 0x3100            ; Terminates and stays resident.
-MOV DX, 0x00FF            ;
+MOV DX, EndTSR            ;
+ADD DX, 0x0F              ;
+SHR DX, 0x04              ;
 INT 0x21                  ;
-
-
 
 IsActive:                 ; Quits if the TSR is already active.
 MOV AH, 0x4C              ;
 INT 0x21                  ;
-
-TSR:
-
-PUSHA                     ; Saves the registers.
-PUSH DS                   ;
-PUSH ES
-
-PUSH FS                   ; Restores the data segment register.
-POP DS                    ;
-
-%INCLUDE "Memdump.asm"    ; Includes the TSR's main body.
-
-POP ES                    ; Restores the registers.
-POP DS                    ;
-POPA                      ;
-
-INT RedirectedTo          ; Calls the redirected interrupt.
-
-IRET                      ; Returns.
