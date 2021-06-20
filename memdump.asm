27c8c7cd8dccb0f6cb65e3fb1523edd9752671e4
Peter Swinkels <peter_swinkels@hotmail.com>
2021-06-20 18:07:12 +0200

    Add files via upload

---
+++ memdump.asm
@@ -0,0 +1,58 @@
+IN AL, 0x60           ; Skips memory dumping unless the F12 key is being pressed.
+CMP AL, 0x58          ;
+JNE Done              ;
+
+MOV AH, 0x3C          ; Creates the output file.
+MOV CX, 0x00          ;
+LEA DX, OutputFile    ;
+INT 0x21              ;
+JC Done               ;
+
+MOV BX, AX            ; Closes the newly created output file.
+MOV AH, 0x3E          ;
+INT 21h               ;
+JC Done               ;
+
+MOV AX, 0x3D01        ; Opens the output file for writing.
+LEA DX, OutputFile    ;
+INT 0x21              ;
+JC Done               ;
+
+MOV BX, AX            ; Retrieves the filehandle.
+
+MOV WORD [MemorySegment], 0x0000    ; Sets the first memory block.
+
+MOV AX, DS   ; Saves the current data segment.
+MOV ES, AX   ;
+
+Dump:
+   ES                          ; Sets the memory block to be written to the output file.
+   MOV AX, [MemorySegment]     ;
+   MOV DS, AX                  ;
+
+   MOV AH, 0x40                ; Writes the memory block to the output file.
+   MOV CX, 0xFFFF              ;
+   MOV DX, 0x0000              ;
+   INT 0x21                    ;
+   JC Done                     ;
+
+   ES                          ; Checks whether the last memory block has been reached.
+   MOV AX, [MemorySegment]     ;
+   CMP AX, 0xF000              ;
+   JAE DumpFinished            ;
+
+   ADD AX, 0x1000              ; Moves to the next memory block.
+   ES                          ;
+   MOV [MemorySegment], AX     ;
+JMP Dump
+
+DumpFinished:
+MOV AH, 0x3E          ; Closes the output file.
+INT 21h               ;
+JMP Done
+
+OutputFile DB "MemDump.dat", 0x00
+MemorySegment DW 0x0000
+
+Done:
+
---
+++ mmdmptsr.asm
@@ -0,0 +1,64 @@
+; Memory Dumping TSR for MS-DOS v1.00 - By: Peter Swinkels, ***2021***
+ORG 0x0100                ; Indicates that all relative pointers to data are moved forward by 0x0100 bytes.
+
+RedirectedFrom EQU 0x08   ; Defines the interrupt to be redirected.
+RedirectedTo EQU 0xFF     ; Defines the redirected interrupt's new vector.
+
+MOV AH, 0x35              ; Checks whether this TSR is already active by checking for a redirected interrupt.
+MOV AL, RedirectedTo      ;
+INT 0x21                  ;
+MOV AX, ES                ;
+CMP AX, 0x0000            ;
+JNE IsActive              ;
+    CMP BX, 0x0000        ;
+    JNE IsActive          ;
+
+MOV AH, 0x35              ; Retrieves vector the vector for the interrupt to be redirected.
+MOV AL, RedirectedFrom    ;
+INT 0x21                  ;
+
+MOV AX, DS                ; Saves the data segment register.
+MOV FS, AX                ;
+
+MOV DX, BX                ; Places the retrieved vector at another interrupt.
+PUSH ES                   ;
+POP DS                    ;
+MOV AH, 0x25              ;
+MOV AL, RedirectedTo      ;
+INT 0x21                  ;
+
+PUSH CS                   ; Sets this TSR's interrupt vector.
+POP DS                    ;
+MOV DX, TSR               ;
+MOV AH, 0x25              ;
+MOV AL, RedirectedFrom    ;
+INT 0x21                  ;
+
+MOV AX, 0x3100            ; Terminates and stays resident.
+MOV DX, 0x00FF            ;
+INT 0x21                  ;
+
+
+
+IsActive:                 ; Quits if the TSR is already active.
+MOV AH, 0x4C              ;
+INT 0x21                  ;
+
+TSR:
+
+PUSHA                     ; Saves the registers.
+PUSH DS                   ;
+PUSH ES
+
+PUSH FS                   ; Restores the data segment register.
+POP DS                    ;
+
+%INCLUDE "Memdump.asm"    ; Includes the TSR's main body.
+
+POP ES                    ; Restores the registers.
+POP DS                    ;
+POPA                      ;
+
+INT RedirectedTo          ; Calls the redirected interrupt.
+
+IRET                      ; Returns.
